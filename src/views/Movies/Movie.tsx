365bc303a2b732c368d3d6a4f36ca689631543f9
abadfish <kirsten.ofarrell@gmail.com>
2021-12-22 11:15:54 -0500

    styled movie page

--- src/views/Movies/Movie.tsx
+++ src/views/Movies/Movie.tsx
@@ -17,15 +17,38 @@ const Movie = () => {
 	console.log(movie)
 
 	return (
-		<div>
+		<MoviePage>
 			<img src={`${ movie?.Poster }`} alt="movie poster" />
-			<h2>{ movie?.Title } - { movie?.Year }</h2>
-			<h3>{ movie?.Genre }</h3>
-			<p>{ movie?.Plot }</p>
-			<p>{ movie?.imdbRating }</p>
-			<button>Add To Favorites</button>
-		</div>
+			<div>
+				<h2>{ movie?.Title } - { movie?.Year }</h2>
+				<h3>{ movie?.Genre }</h3>
+				<p>{ movie?.Plot }</p>
+				<p>IMDB Rating: { movie?.imdbRating }</p>
+				<button>Add To Favorites</button>
+			</div>
+		</MoviePage>
 	)
 }
 
 export default Movie
+
+const MoviePage = styled.main `
+	padding: 4rem 6rem;
+	display: flex;
+	img {
+		height: 250px;
+		width: auto;
+	}
+	div {
+		padding: 0 2rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		h2 {
+			margin-top: 0;
+		}
+		button {
+			width: 200px;
+		}
+	}
+`
